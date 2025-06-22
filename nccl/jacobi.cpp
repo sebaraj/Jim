fadda9efdfa84ed8231a98dda65a79e4bc59e5b2
Bryan SebaRaj <bryanwsebaraj@gmail.com>
2025-06-22 00:15:03 -0400

    nccl + mpi init

--- nccl/jacobi.cpp
+++ nccl/jacobi.cpp
@@ -250,14 +250,6 @@ bool get_argbool(char** begin, char** end, const std::string& arg) {
 
 int main(int argc, char* argv[]) {
     MPI_CALL(MPI_Init(&argc, &argv));
-    // awareness check?
-#if !defined(SKIP_CUDA_AWARENESS_CHECK) && defined(MPIX_CUDA_AWARE_SUPPORT)
-    if (1 != MPIX_Query_cuda_support()) {
-        fprintf(stderr, "The used MPI Implementation does not have CUDA-aware support enabled!\n");
-        MPI_CALL(MPI_Finalize());
-        return -1;
-    }
-#endif
     int rank;
     MPI_CALL(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
     int size;
@@ -265,194 +257,53 @@ int main(int argc, char* argv[]) {
     int num_devices = 0;
     CUDA_RT_CALL(cudaGetDeviceCount(&num_devices));
 
+    ncclUniqueId nccl_uid;
+    if (rank == 0) NCCL_CALL(ncclGetUniqueId(&nccl_uid));
+    MPI_CALL(MPI_Bcast(&nccl_uid, sizeof(ncclUniqueId), MPI_BYTE, 0, MPI_COMM_WORLD));
+    MPI_CALL(MPI_Barrier(MPI_COMM_WORLD));
+
     const int iter_max = get_argval<int>(argv, argv + argc, "-niter", 1000);
     const int nccheck = get_argval<int>(argv, argv + argc, "-nccheck", 1);
     const int nx = get_argval<int>(argv, argv + argc, "-nx", 16384);
     const int ny = get_argval<int>(argv, argv + argc, "-ny", 16384);
     const bool csv = get_argbool(argv, argv + argc, "-csv");
+    bool user_buffer_reg = get_argval(argv, argv + argc, "-user_buffer_reg");
+#if NCCL_UB_SUPPORT == 0
+    if (user_buffer_reg) {
+        fprintf(stderr,
+                "WARNING: Ignoring -user_buffer_reg, required NCCL APIs are provided by NCCL.\n");
+        user_buffer_reg = false;
+    }
+#endif  // NCCL_UB_SUPPORT == 0
 
     int local_rank = -1;
+    int local_size = 1;
     {
         MPI_Comm local_comm;
         MPI_CALL(MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank, MPI_INFO_NULL,
                                      &local_comm));
         MPI_CALL(MPI_Comm_rank(local_comm, &local_rank));
+        MPI_CALL(MPI_Comm_size(local_comm, &local_size));
+
         MPI_CALL(MPI_Comm_free(&local_comm));
     }
 
-    CUDA_RT_CALL(cudaSetDevice(local_rank % num_devices));
+    if (1 < num_devices && num_devices < local_size) {
+        fprintf(
+            stderr,
+            "ERROR Number of visible devices (%d) is less than number of ranks on the node (%d)!\n",
+            num_devices, local_size);
+        MPI_CALL(MPI_Finalize());
+        return 1;
+    }
+    if (1 == num_devices) {
+        // Only 1 device visbile assuming GPU affinity is handled via CUDA_VISIBLE_DEVICES
+        CUDA_RT_CALL(cudaSetDevice(0));
+    } else {
+        CUDA_RT_CALL(cudaSetDevice(local_rank));
+    }
     CUDA_RT_CALL(cudaFree(0));
 
-    real* a_ref_h;
-    CUDA_RT_CALL(cudaMallocHost(&a_ref_h, nx * ny * sizeof(real)));
-    real* a_h;
-    CUDA_RT_CALL(cudaMallocHost(&a_h, nx * ny * sizeof(real)));
-    double runtime_serial = single_gpu(nx, ny, iter_max, a_ref_h, nccheck, !csv && (0 == rank));
-
-    int chunk_size;
-    int chunk_size_low = (ny - 2) / size;
-    int chunk_size_high = chunk_size_low + 1;
-    int num_ranks_low = size * chunk_size_low + size - (ny - 2);
-    if (rank < num_ranks_low)
-        chunk_size = chunk_size_low;
-    else
-        chunk_size = chunk_size_high;
-
-    real* a;
-    CUDA_RT_CALL(cudaMalloc(&a, nx * (chunk_size + 2) * sizeof(real)));
-    real* a_new;
-    CUDA_RT_CALL(cudaMalloc(&a_new, nx * (chunk_size + 2) * sizeof(real)));
-
-    CUDA_RT_CALL(cudaMemset(a, 0, nx * (chunk_size + 2) * sizeof(real)));
-    CUDA_RT_CALL(cudaMemset(a_new, 0, nx * (chunk_size + 2) * sizeof(real)));
-
-    int iy_start_global;
-    if (rank < num_ranks_low) {
-        iy_start_global = rank * chunk_size_low + 1;
-    } else {
-        iy_start_global
-            = num_ranks_low * chunk_size_low + (rank - num_ranks_low) * chunk_size_high + 1;
-    }
-
-    int iy_end_global = iy_start_global + chunk_size - 1;  // My last index in the global array
-
-    int iy_start = 1;
-    int iy_end = iy_start + chunk_size;
-
-    // Set diriclet boundary conditions on left and right boarder
-    launch_initialize_boundaries(a, a_new, PI, iy_start_global - 1, nx, (chunk_size + 2), ny);
-    CUDA_RT_CALL(cudaDeviceSynchronize());
-
-    cudaStream_t compute_stream;
-    CUDA_RT_CALL(cudaStreamCreate(&compute_stream));
-    cudaEvent_t compute_done;
-    CUDA_RT_CALL(cudaEventCreateWithFlags(&compute_done, cudaEventDisableTiming));
-
-    real* l2_norm_d;
-    CUDA_RT_CALL(cudaMalloc(&l2_norm_d, sizeof(real)));
-    real* l2_norm_h;
-    CUDA_RT_CALL(cudaMallocHost(&l2_norm_h, sizeof(real)));
-
-    PUSH_RANGE("MPI_Warmup", 5)
-    for (int i = 0; i < 10; ++i) {
-        const int top = rank > 0 ? rank - 1 : (size - 1);
-        const int bottom = (rank + 1) % size;
-        MPI_CALL(MPI_Sendrecv(a_new + iy_start * nx, nx, MPI_REAL_TYPE, top, 0,
-                              a_new + (iy_end * nx), nx, MPI_REAL_TYPE, bottom, 0, MPI_COMM_WORLD,
-                              MPI_STATUS_IGNORE));
-        MPI_CALL(MPI_Sendrecv(a_new + (iy_end - 1) * nx, nx, MPI_REAL_TYPE, bottom, 0, a_new, nx,
-                              MPI_REAL_TYPE, top, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
-        std::swap(a_new, a);
-    }
-    POP_RANGE
-
-    CUDA_RT_CALL(cudaDeviceSynchronize());
-
-    if (!csv && 0 == rank) {
-        printf(
-            "Jacobi relaxation: %d iterations on %d x %d mesh with norm check "
-            "every %d iterations\n",
-            iter_max, ny, nx, nccheck);
-    }
-
-    int iter = 0;
-    real l2_norm = 1.0;
-    bool calculate_norm = true;
-
-    MPI_CALL(MPI_Barrier(MPI_COMM_WORLD));
-    double start = MPI_Wtime();
-    PUSH_RANGE("Jacobi solve", 0)
-    while (l2_norm > tol && iter < iter_max) {
-        CUDA_RT_CALL(cudaMemsetAsync(l2_norm_d, 0, sizeof(real), compute_stream));
-
-        calculate_norm = (iter % nccheck) == 0 || (!csv && (iter % 100) == 0);
-
-        launch_jacobi_kernel(a_new, a, l2_norm_d, iy_start, iy_end, nx, calculate_norm,
-                             compute_stream);
-        CUDA_RT_CALL(cudaEventRecord(compute_done, compute_stream));
-
-        if (calculate_norm) {
-            CUDA_RT_CALL(cudaMemcpyAsync(l2_norm_h, l2_norm_d, sizeof(real), cudaMemcpyDeviceToHost,
-                                         compute_stream));
-        }
-
-        const int top = rank > 0 ? rank - 1 : (size - 1);
-        const int bottom = (rank + 1) % size;
-
-        // Apply periodic boundary conditions
-        CUDA_RT_CALL(cudaEventSynchronize(compute_done));
-        PUSH_RANGE("MPI", 5)
-        MPI_CALL(MPI_Sendrecv(a_new + iy_start * nx, nx, MPI_REAL_TYPE, top, 0,
-                              a_new + (iy_end * nx), nx, MPI_REAL_TYPE, bottom, 0, MPI_COMM_WORLD,
-                              MPI_STATUS_IGNORE));
-        MPI_CALL(MPI_Sendrecv(a_new + (iy_end - 1) * nx, nx, MPI_REAL_TYPE, bottom, 0, a_new, nx,
-                              MPI_REAL_TYPE, top, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE));
-        POP_RANGE
-
-        if (calculate_norm) {
-            CUDA_RT_CALL(cudaStreamSynchronize(compute_stream));
-            MPI_CALL(MPI_Allreduce(l2_norm_h, &l2_norm, 1, MPI_REAL_TYPE, MPI_SUM, MPI_COMM_WORLD));
-            l2_norm = std::sqrt(l2_norm);
-
-            if (!csv && 0 == rank && (iter % 100) == 0) {
-                printf("%5d, %0.6f\n", iter, l2_norm);
-            }
-        }
-
-        std::swap(a_new, a);
-        iter++;
-    }
-    double stop = MPI_Wtime();
-    POP_RANGE
-
-    CUDA_RT_CALL(cudaMemcpy(a_h + iy_start_global * nx, a + nx,
-                            std::min((ny - iy_start_global) * nx, chunk_size * nx) * sizeof(real),
-                            cudaMemcpyDeviceToHost));
-
-    int result_correct = 1;
-    for (int iy = iy_start_global; result_correct && (iy < iy_end_global); ++iy) {
-        for (int ix = 1; result_correct && (ix < (nx - 1)); ++ix) {
-            if (std::fabs(a_ref_h[iy * nx + ix] - a_h[iy * nx + ix]) > tol) {
-                fprintf(stderr,
-                        "ERROR on rank %d: a[%d * %d + %d] = %f does not match %f "
-                        "(reference)\n",
-                        rank, iy, nx, ix, a_h[iy * nx + ix], a_ref_h[iy * nx + ix]);
-                result_correct = 0;
-            }
-        }
-    }
-
-    int global_result_correct = 1;
-
-    MPI_CALL(MPI_Allreduce(&result_correct, &global_result_correct, 1, MPI_INT, MPI_MIN,
-                           MPI_COMM_WORLD));
-    result_correct = global_result_correct;
-
-    if (rank == 0 && result_correct) {
-        if (csv) {
-            printf("mpi, %d, %d, %d, %d, %d, 1, %f, %f\n", nx, ny, iter_max, nccheck, size,
-                   (stop - start), runtime_serial);
-        } else {
-            printf("Num GPUs: %d.\n", size);
-            printf(
-                "%dx%d: 1 GPU: %8.4f s, %d GPUs: %8.4f s, speedup: %8.2f, "
-                "efficiency: %8.2f \n",
-                ny, nx, runtime_serial, size, (stop - start), runtime_serial / (stop - start),
-                runtime_serial / (size * (stop - start)) * 100);
-        }
-    }
-    CUDA_RT_CALL(cudaEventDestroy(compute_done));
-    CUDA_RT_CALL(cudaStreamDestroy(compute_stream));
-
-    CUDA_RT_CALL(cudaFreeHost(l2_norm_h));
-    CUDA_RT_CALL(cudaFree(l2_norm_d));
-
-    CUDA_RT_CALL(cudaFree(a_new));
-    CUDA_RT_CALL(cudaFree(a));
-
-    CUDA_RT_CALL(cudaFreeHost(a_h));
-    CUDA_RT_CALL(cudaFreeHost(a_ref_h));
-
-    MPI_CALL(MPI_Finalize());
+    // TODO:
     return !result_correct;
 }
